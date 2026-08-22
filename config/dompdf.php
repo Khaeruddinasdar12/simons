f4f119d7a7ee96663d675fa57c516f8327847075
Khaeruddin Asdar <46445417+Khaeruddinasdar12@users.noreply.github.com>
2026-08-22 21:37:00 +0800

    fix format sk penguji, font diperbesar

--- config/dompdf.php
+++ config/dompdf.php
@@ -286,7 +286,7 @@ return [
         /**
          * A ratio applied to the fonts height to be more like browsers' line height
          */
-        'font_height_ratio' => 1.0,
+        'font_height_ratio' => 1.05,
 
         /**
          * Use the HTML5 Lib parser
--- resources/views/sk/partials/document-css.blade.php
+++ resources/views/sk/partials/document-css.blade.php
@@ -1,42 +1,42 @@
-        @page { margin: 10mm 14mm 10mm 14mm; size: 210mm 330mm; }
+        @page { margin: 10mm 16mm 10mm 16mm; size: 210mm 330mm; }
         body {
             font-family: {{ empty($isBrowserPreview) ? 'Times, serif' : '"Times New Roman", Times, serif' }};
-            font-size: 9pt;
+            font-size: 9.5pt;
             color: #000;
-            line-height: 1.16;
+            line-height: 1.18;
         }
         p { margin: 0; }
         .center { text-align: center; }
-        .logo { width: 40px; height: auto; margin-bottom: 1px; }
+        .logo { width: 48px; height: auto; margin-bottom: 2px; }
         .header-title {
             font-weight: bold;
-            font-size: 10pt;
-            line-height: 1.12;
+            font-size: 10.5pt;
+            line-height: 1.14;
             margin: 0;
         }
         .nomor {
-            margin: 3px 0 2px;
+            margin: 4px 0 2px;
             font-weight: bold;
-            font-size: 9pt;
+            font-size: 9.5pt;
         }
         .tentang {
             font-weight: bold;
             text-transform: uppercase;
-            margin: 1px 0 3px;
-            font-size: 9pt;
+            margin: 1px 0 4px;
+            font-size: 9.5pt;
             line-height: 1.15;
         }
         .bismillah {
             font-weight: bold;
-            margin: 3px 0 2px;
-            font-size: 9pt;
+            margin: 4px 0 2px;
+            font-size: 9.5pt;
         }
         .intro {
             font-weight: bold;
-            margin: 0 0 5px;
-            font-size: 9pt;
+            margin: 0 0 6px;
+            font-size: 9.5pt;
             text-transform: uppercase;
-            line-height: 1.12;
+            line-height: 1.14;
         }
         table.meta,
         table.dict {
@@ -47,26 +47,28 @@
         table.meta td,
         table.dict td {
             vertical-align: top;
-            padding: 1.5px 0;
+            padding: 2px 0;
             text-align: left;
         }
         table.meta td.label,
         table.dict td.dict-key {
-            width: 120px;
+            width: 125px;
             font-weight: bold;
             white-space: nowrap;
-            padding-right: 4px;
+            padding-right: 8px;
         }
         table.meta td.colon,
         table.dict td.dict-colon {
-            width: 14px;
+            width: 16px;
+            padding-right: 8px;
+            text-align: center;
         }
         .bold { font-weight: bold; }
         .memutuskan {
             text-align: center;
             font-weight: bold;
-            font-size: 10pt;
-            margin: 4px 0 3px;
+            font-size: 10.5pt;
+            margin: 5px 0 3px;
             letter-spacing: 1px;
         }
         table.kv,
@@ -78,19 +80,23 @@
         table.kv td,
         table.olist td {
             vertical-align: top;
-            padding: 0.5px 0;
+            padding: 1px 0;
             text-align: left;
         }
         table.kv td.k {
-            width: 110px;
+            width: 118px;
             white-space: nowrap;
+            padding-right: 8px;
         }
         table.kv td.c {
-            width: 14px;
+            width: 16px;
+            padding-right: 8px;
+            text-align: center;
         }
         table.olist td.n {
-            width: 18px;
+            width: 22px;
             white-space: nowrap;
+            padding-right: 6px;
         }
         .end-block {
             width: 100%;
@@ -103,28 +109,28 @@
         .ttd-box .nama {
             font-weight: bold;
             text-decoration: underline;
-            margin-top: 1px;
-            font-size: 9pt;
+            margin-top: 2px;
+            font-size: 9.5pt;
         }
         .qr-ttd {
-            width: 42px;
-            height: 42px;
+            width: 48px;
+            height: 48px;
             margin: 2px 0;
         }
         .footer-qr {
             margin-top: 4px;
             padding-top: 3px;
             border-top: 1px solid #999;
-            font-size: 7.5pt;
-            line-height: 1.15;
+            font-size: 8pt;
+            line-height: 1.18;
         }
         .footer-qr img {
-            width: 40px;
-            height: 40px;
+            width: 44px;
+            height: 44px;
             vertical-align: middle;
-            margin-right: 6px;
+            margin-right: 8px;
         }
-        .small { font-size: 8pt; line-height: 1.16; }
+        .small { font-size: 9pt; line-height: 1.18; }
         .preview-banner {
             background: #fff3cd;
             border: 1px solid #f0c36d;
